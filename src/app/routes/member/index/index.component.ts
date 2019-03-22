cd843d328923a16c62e74778b9a0bd903c79d70f
YangFan17 <173838362@qq.com>
2019-03-22 18:54:16 +0800

    git流程学习-xlb

--- src/app/routes/member/index/index.component.ts
+++ src/app/routes/member/index/index.component.ts
@@ -24,7 +24,7 @@ export class MemberComponent extends PagedListingComponentBase<Member>{
 
   protected fetchData(request: PagedRequestDto, pageNumber: number, finishedCallback: Function): void {
     this.memberService.getAll(this.keyWord, request.skipCount, request.maxResultCount).finally(() => {
-      console.log('首页数据刷新1');
+      console.log('首页数据刷新2');
       finishedCallback();
     }).then((res) => {
       if (res) {
